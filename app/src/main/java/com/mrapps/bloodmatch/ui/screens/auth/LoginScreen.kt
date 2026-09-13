fc2073ed22050d45a77388536ce205e8f04c5288
mugdha <mugdharahman11@gmail.com>
2026-09-13 17:50:02 +0600

    Optimize imports in `LoginScreen.kt` for better structure and readability

--- app/src/main/java/com/mrapps/bloodmatch/ui/screens/auth/LoginScreen.kt
+++ app/src/main/java/com/mrapps/bloodmatch/ui/screens/auth/LoginScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bloodtype
+
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -13,6 +14,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+
 import com.mrapps.bloodmatch.ui.auth.AuthState
 import com.mrapps.bloodmatch.ui.auth.AuthViewModel
 import com.mrapps.bloodmatch.ui.theme.BloodButton
